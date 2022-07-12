65b98e9a898c180e5609d360daeb0fa0074ce07b
Michael <michaelbui96@outlook.com>
2022-07-11 22:44:12 -0700

    tags pointer

--- components/LabSmallTags.js
+++ components/LabSmallTags.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const LabTagSmall = ({ text }) => {
   return (
     <Link href={`/learn/${kebabCase(text)}`}>
-      <span className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+      <span className="mr-3 cursor-pointer text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
         {text.split(' ').join('-')}
       </span>
     </Link>
--- components/projectTag.js
+++ components/projectTag.js
@@ -3,7 +3,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 
 const ProjectTag = ({ text }) => {
   return (
-    <span className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+    <span className="mr-3 cursor-pointer text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
       {text.split(' ').join('-')}
     </span>
   )
