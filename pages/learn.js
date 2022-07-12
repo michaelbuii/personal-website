15b5758847b9c79388b926901905d45c61ee1c3b
Michael <michaelbui96@outlook.com>
2022-07-12 04:29:07 -0700

    style change

--- pages/learn.js
+++ pages/learn.js
@@ -25,7 +25,7 @@ export default function Tags({ tags, posts, pagination, initialDisplayPosts }) {
   const sortedTags = Object.keys(tags).sort((a, b) => tags[b] - tags[a])
   return (
     <>
-      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-2 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
         <div className="space-x-2 pb-0 md:space-y-5">
           <h1 className="text-3xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:border-r-2 md:px-6 md:text-3xl md:leading-10">
             Categories
